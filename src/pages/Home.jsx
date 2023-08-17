3b723c7f9ff6b53898b20130100b3cb296500c02
KamilArici <kamilarici5@gmail.com>
2023-08-17 23:13:43 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+import KpiCards from "../components/KpiCards";
+import Charts from "../components/Charts";
+const Home = () => {
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={2}>
+        Dashboard
+      </Typography>
+
+      <KpiCards />
+
+      <Charts />
+    </div>
+  );
+};
+
+export default Home;
