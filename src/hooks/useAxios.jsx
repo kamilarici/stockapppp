0a996bede0ffc997d9496e7da234714fbeac6165
KamilArici <kamilarici5@gmail.com>
2023-08-25 22:04:51 +0300

    edit2

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,19 +1,19 @@
-import { useSelector } from "react-redux"
-import axios from "axios"
-
+import { useSelector } from "react-redux";
+import axios from "axios";
+//? axios instense klasörüdür kullanılan axiosları ortak yapı içine alınıp gerekli yerlerda k
 const useAxios = () => {
-  const { token } = useSelector((state) => state.auth)
+  const { token } = useSelector((state) => state.auth);
 
   const axiosWithToken = axios.create({
     baseURL: `${import.meta.env.VITE_BASE_URL}`,
     headers: { Authorization: `Token ${token}` },
-  })
+  });
 
   const axiosPublic = axios.create({
     baseURL: `${import.meta.env.VITE_BASE_URL}`,
-  })
+  });
 
-  return { axiosWithToken, axiosPublic }
-}
+  return { axiosWithToken, axiosPublic };
+};
 
-export default useAxios
+export default useAxios;
